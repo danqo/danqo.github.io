49e48deba21ad8f7f65f37e51f15004cef06a668
Daniel Wang <danielwang@Daniels-MacBook-Pro.local>
2024-09-21 22:21:56 -0700

    setup for page routing and linting

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Daniel Wang</h1>
+    </>
+  );
+};
+
+export default React.memo(Home);
---
+++ src/pages/Home/index.ts
@@ -0,0 +1,2 @@
+export { default } from './Home';
+// export type { HomePageProps } from './HomePageTypes';
